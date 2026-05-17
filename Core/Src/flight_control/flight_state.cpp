62c416e2abb54dfc5ed9a8cbc5b1db28693a7c4a
5ozone <lileofye@gmail.com>
2026-05-17 18:52:20 +0900

    flight control에 있는 ignition_count 삭제 및 main에 ignite_counter-> ignition_count로 변경

---
+++ Core/Src/flight_control/flight_state.cpp
@@ -0,0 +1,159 @@
+/*
+ * flight_state.cpp
+ *
+ *  Created on: Apr 1, 2026
+ *      Author: shpar
+ */
+
+#include "flight_control/flight_state.h"
+
+#include "utils/debug_printf.h"
+#include "utils/common.h"
+
+static FLIGHT_STATE flight_state = FLIGHT_STATE_READY;
+
+static float ground_ax, ground_ay, ground_az;
+static float ground_a;
+
+float ground_pres = 0;
+static float last_pressure[GROUND_PRESSURE_NUM];
+static int last_pressure_idx = 0;
+static int last_pressure_count = 0;
+static float max_alt = 0;
+static float alt = 0.0;
+static int lower_count = 0, outlier_count = 0;
+
+MILLIS_TIMER_DEFINE(drogue);
+MILLIS_TIMER_DEFINE(main);
+MILLIS_TIMER_DEFINE(touchdown);
+
+static uint32_t main_time_millis;
+
+FLIGHT_STATE init_flight_state_machine(const flight_state_machine_init_input &init_input){
+	ground_ax = init_input.ground_ax;
+	ground_ay = init_input.ground_ay;
+	ground_az = init_input.ground_az;
+	ground_a = norm(ground_ax, ground_ay, ground_az);
+	flight_state = FLIGHT_STATE_READY;
+	return flight_state;
+}
+
+FLIGHT_STATE run_flight_state_machine(const flight_state_machine_input &input){
+	if(flight_state > FLIGHT_STATE_READY){
+		//compute alt after ground_pres is obtained(after launch)
+		alt = Pa2altM(input.pressure, ground_pres);
+		//check outlier
+		if(alt > max_alt + 10){
+			outlier_count++;
+			if(outlier_count >= 3) outlier_count = 0;
+		}
+		else outlier_count = 0;
+		//outlier_count == 0 => apply to max_alt
+		if(outlier_count == 0){
+			if(alt > max_alt){
+				max_alt = alt;
+				lower_count = 0;
+			}
+			else if(alt < max_alt - 2.0) lower_count++;
+		}
+	}
+	else{
+		last_pressure[last_pressure_idx++] = input.pressure;
+		if(last_pressure_idx == GROUND_PRESSURE_NUM) last_pressure_idx = 0;
+		last_pressure_count++;
+		alt = 0.0;
+	}
+
+	//================================================
+
+	//flight state management
+	switch(flight_state){
+	case FLIGHT_STATE_READY:
+		//IMU의 배치 방향이 달라질 수 있으므로, 정사영 >= 2g로 판단
+		if((input.acc_x * ground_ax + input.acc_y * ground_ay + input.acc_z * ground_az)
+			/ground_a >= 2000){
+			ground_pres = 0.0;
+			for(int i = 0; i < min(last_pressure_count, GROUND_PRESSURE_NUM); i++){
+				ground_pres += last_pressure[i];
+			}
+			ground_pres /= min(last_pressure_count, GROUND_PRESSURE_NUM);
+			flight_state = FLIGHT_STATE_LAUNCH;
+			lower_count = 0;
+			outlier_count = 0;
+			max_alt = 0;
+#ifdef RECOVERY_TEST
+			if(!MILLIS_TIMER_ISSET(drogue)){
+				MILLIS_TIMER_SET(drogue);
+			}
+#endif
+		}
+		break;
+
+	case FLIGHT_STATE_LAUNCH:
+		//공전회 --> 30초, 실제 발사에서 주석 부분
+		if (
+#ifdef RECOVERY_TEST
+//	#ifdef FC1
+		MILLIS_TIMER_CHECK(drogue, 1000*30)
+//	#else
+//		MILLIS_TIMER_CHECK(drogue, 1000*30 + 700)
+//	#endif
+#else
+		lower_count >= 5
+#ifdef REAL_LAUNCH
+		&& alt >= DROGUE_DEPLOY_MIN_ALT
+#endif
+#endif
+		){
+#ifdef FC1
+			if(!MILLIS_TIMER_ISSET(main)){
+				MILLIS_TIMER_SET(main);
+			}
+			flight_state = FLIGHT_STATE_DROGUE_PARACHUTE_DEPLOYED;
+
+#else
+			if(!MILLIS_TIMER_ISSET(main)){
+				MILLIS_TIMER_SET(main);
+			}
+#endif
+		}
+#ifdef FC2
+		if(MILLIS_TIMER_CHECK(main, 700)){
+			flight_state = FLIGHT_STATE_DROGUE_PARACHUTE_DEPLOYED;
+		}
+#endif
+		break;
+
+	case FLIGHT_STATE_DROGUE_PARACHUTE_DEPLOYED:
+		if(
+#ifdef RECOVERY_TEST
+	#ifdef FC1
+		MILLIS_TIMER_CHECK(main, 1000*30)
+	#else
+		MILLIS_TIMER_CHECK(main, 1000*30 /*+ 700*/)
+	#endif
+#endif
+		MILLIS_TIMER_CHECK(main, 5*1000) && alt <= MAIN_DEPLOY_ALT
+		){
+//			main_time_millis = input.time_millis;
+			if(!MILLIS_TIMER_ISSET(touchdown)){
+				MILLIS_TIMER_SET(touchdown);
+			}
+			flight_state=FLIGHT_STATE_MAIN_PARACHUTE_DEPLOYED;
+		}
+		break;
+
+	case FLIGHT_STATE_MAIN_PARACHUTE_DEPLOYED:
+		if(
+//				input.time_millis > main_time_millis + 10*1000
+				MILLIS_TIMER_CHECK(touchdown, 10*1000)
+		){ //10 sec
+			flight_state = FLIGHT_STATE_TOUCHDOWN;
+		}
+		break;
+	default:
+		break;
+	}
+	
+	return flight_state;
+}
---
+++ Core/Src/stm32_main.cpp
@@ -0,0 +1,288 @@
+/*
+ * stm32_main.cpp
+ *
+ *  Created on: Apr 1, 2026
+ *      Author: shpar
+ */
+
+#include "stm32_main.h"
+#include "main.h"
+
+#include "baro/bmp581.h"
+#include "imu/icm20948.h"
+#include "SD/SD.h"
+#include "flight_control/flight_state.h"
+#include "utils/micros.h"
+#include "utils/stm32_uart_stdio.h"
+#include "utils/debug_printf.h"
+#include "utils/common.h"
+#include "config.h"
+#include "buzzer/switching_buzzer.h"
+#include "buzzer/buzzer.h"
+#include "ADC/ADC.h"
+#include "quaternion.h"
+
+extern ADC_HandleTypeDef hadc1;
+extern ADC_HandleTypeDef hadc2;
+
+extern I2C_HandleTypeDef hi2c1;
+
+extern SPI_HandleTypeDef hspi1;
+extern SPI_HandleTypeDef hspi2;
+
+extern TIM_HandleTypeDef htim2;
+extern TIM_HandleTypeDef htim3;
+
+extern UART_HandleTypeDef huart2;
+
+static int imu_cnt = 0;
+static int imu_dmp_cnt = 0;
+static int bmp_cnt = 0;
+static uint32_t hzprint_timer_millis;
+
+static icm20948_agmt_t icm20948_last_agmt;
+static icm20948_dmp_quat_t icm20948_last_dmp_quat;
+static bmp5_sensor_data bmp581_last_data;
+static int passive;
+extern float ground_pres;
+
+static int ignition_count;
+#ifdef FC2
+MILLIS_TIMER_DEFINE(ignite);
+#endif
+bool is_ignited;
+
+static FLIGHT_STATE flight_state;
+
+void measure_databoard_voltage(){
+	//initial sampling is not accurate
+	for(int i = 0;i < 10;i++){
+		analogRead(&DATABOARD_ADC_HANDLE);
+		HAL_Delay(100);
+	}
+
+	float voltage = convert_adc_to_voltate(analogRead(&DATABOARD_ADC_HANDLE));
+	DEBUG_PRINT("volt: %f\r\n", voltage);
+
+	int volt_n = floor(voltage*10);
+	int volt_cnt1 = volt_n/10, volt_cnt2 = volt_n%10;
+	for(int i=0;i<volt_cnt1;i++){
+		tone(400);
+		HAL_Delay(250);
+		noTone();
+		HAL_Delay(250);
+	}
+	noTone();
+	HAL_Delay(3000);
+	for(int i=0;i<volt_cnt2;i++){
+		tone(400);
+		HAL_Delay(500);
+		noTone();
+		HAL_Delay(250);
+	}
+	noTone();
+	HAL_Delay(3000);
+}
+
+
+void begin(void){
+	DWT_Init();
+	init_stdio();
+	DEBUG_PRINT("start initializing...\r\n");
+	init_buzzer(&htim3);
+	tone(150);
+	while(!init_imu(&hi2c1)) {
+		DEBUG_PRINT("IMU initialize failed. Trying again...\r\n");
+	}
+	tone(200);
+	init_baro(&hspi2, BMP_CS_GPIO_Port, BMP_CS_Pin);
+	tone(250);
+	init_sd(&hspi1, SD_CS_GPIO_Port, SD_CS_Pin);
+	tone(300);
+	init_data_csv("launch_04_05");
+	measure_databoard_voltage();
+
+	HAL_GPIO_WritePin(IGNITE_GPIO_Port, IGNITE_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(DROGUE_SRAD_GPIO_Port, DROGUE_SRAD_Pin, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(MAIN_SRAD_GPIO_Port, MAIN_SRAD_Pin, GPIO_PIN_RESET);
+
+	if(!sd_fail_flag) buzzer_play_ok();
+	else buzzer_play_fail();
+
+	init_switching_buzzer(&htim2);
+	set_buzzer_switching_intv(1000);
+	DEBUG_PRINT("initialization success!\r\n");
+
+	hzprint_timer_millis = millis();
+
+	//measure ground g vector
+	float ground_ax, ground_ay, ground_az, ground_a;
+	icm20948_agmt_t icm20948_agmt;
+	do {
+		ground_ax = ground_ay = ground_az = 0;
+		for(int i = 0; i < 16; i++){
+			while(!imu_is_agmt_data_ready()) ;
+			imu_get_agmt(&icm20948_agmt);
+			ground_ax += icm20948_agmt.acc_x;
+			ground_ay += icm20948_agmt.acc_y;
+			ground_az += icm20948_agmt.acc_z;
+			HAL_Delay(100);
+		}
+		ground_ax /= 16;
+		ground_ay /= 16;
+		ground_az /= 16;
+		ground_a = norm(ground_ax, ground_ay, ground_az);
+	} while(fabs(ground_a - 1000) > 200);
+
+	flight_state_machine_init_input init_input = {
+		.ground_ax = ground_ax,
+		.ground_ay = ground_ay,
+		.ground_az = ground_az
+	};
+	flight_state = init_flight_state_machine(init_input);
+	set_buzzer_switching_intv(state_buzzer_intv_millis[flight_state]);
+
+	while(!imu_is_agmt_data_ready()) ;
+	imu_get_agmt(&icm20948_last_agmt);
+
+	bmp5_sensor_data bmp_data;
+	while(bmp581.getSensorData(&bmp_data) != BMP5_OK);
+	bmp581_last_data = bmp_data;
+
+	is_ignited = false;
+	ignition_count = 0;
+}
+
+
+void loop(void){
+	if(imu_is_agmt_data_ready()){
+		imu_get_agmt(&icm20948_last_agmt);
+		imu_cnt++;
+		if (flight_state == FLIGHT_STATE_LAUNCH) {
+			float alt = Pa2altM(bmp581_last_data.pressure, ground_pres);
+#ifdef REAL_LAUNCH
+			if (alt > IGNITE_MIN_ALT) {
+#endif
+				if (norm(icm20948_last_agmt.acc_x, icm20948_last_agmt.acc_y, icm20948_last_agmt.acc_z) <= 2000) {
+					ignition_count++;
+				} else {
+					ignition_count = 0;
+				}
+#ifdef REAL_LAUNCH
+			}
+#endif
+		}
+	}
+
+#ifdef ICM20948_USE_DMP
+	icm20948_dmp_data_t icm20948_dmp_data;
+	if(imu_get_dmp_data(&icm20948_dmp_data) && get_quaternion_from_dmp_data(icm20948_dmp_data, &icm20948_last_dmp_quat)){
+//		DEBUG_PRINT("dmp quat: w: %f, x: %f, y: %f, z: %f\r\n", icm20948_last_dmp_quat.w, icm20948_last_dmp_quat.x, icm20948_last_dmp_quat.y, icm20948_last_dmp_quat.z);
+#ifdef FC_DEBUG
+		Quaternion q = Quaternion(icm20948_last_dmp_quat.w, icm20948_last_dmp_quat.x, icm20948_last_dmp_quat.y, icm20948_last_dmp_quat.z);
+		float zx = 0, zy = 0, zz = 1;
+		q.rotateVector(zx, zy, zz);
+		DEBUG_PRINT("quzt9 z axis: (%f, %f, %f)\r\n", zx, zy, zz);
+#endif
+		imu_dmp_cnt++;
+	}
+#endif
+
+	bmp5_sensor_data bmp_data;
+	if(bmp581.getSensorData(&bmp_data) == BMP5_OK){
+		bmp581_last_data = bmp_data;
+		bmp_cnt++;
+	}
+
+	if (HAL_GPIO_ReadPin(PASSIVE_GPIO_Port, PASSIVE_Pin) == GPIO_PIN_RESET) {
+		passive = 1; // connected with GND -> before separation(1)
+	} else {
+		passive = 0; // changed to HIGH -> after separation(0)
+	}
+
+	flight_state_machine_input input = {
+		.time_millis = millis(),
+		.acc_x = icm20948_last_agmt.acc_x,
+		.acc_y = icm20948_last_agmt.acc_y,
+		.acc_z = icm20948_last_agmt.acc_z,
+		.gyr_x = icm20948_last_agmt.gyr_x,
+		.gyr_y = icm20948_last_agmt.gyr_y,
+		.gyr_z = icm20948_last_agmt.gyr_z,
+		.mag_x = icm20948_last_agmt.mag_x,
+		.mag_y = icm20948_last_agmt.mag_y,
+		.mag_z = icm20948_last_agmt.mag_z,
+		.quat_w = icm20948_last_dmp_quat.w,
+		.quat_x = icm20948_last_dmp_quat.x,
+		.quat_y = icm20948_last_dmp_quat.y,
+		.quat_z = icm20948_last_dmp_quat.z,
+		.pressure = bmp581_last_data.pressure,
+		.temperature = bmp581_last_data.temperature,
+		.passive = passive
+	};
+
+	FLIGHT_STATE next_flight_state = run_flight_state_machine(input);
+
+	if(flight_state != next_flight_state){
+		flight_state = next_flight_state;
+		set_buzzer_switching_intv(state_buzzer_intv_millis[flight_state]);
+		if(flight_state == FLIGHT_STATE_DROGUE_PARACHUTE_DEPLOYED) {
+			HAL_GPIO_WritePin(DROGUE_SRAD_GPIO_Port, DROGUE_SRAD_Pin, GPIO_PIN_SET);
+		}
+		else if(flight_state == FLIGHT_STATE_MAIN_PARACHUTE_DEPLOYED) {
+			HAL_GPIO_WritePin(DROGUE_SRAD_GPIO_Port, DROGUE_SRAD_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(MAIN_SRAD_GPIO_Port, MAIN_SRAD_Pin, GPIO_PIN_SET);
+		}
+		else if(flight_state == FLIGHT_STATE_TOUCHDOWN) {
+			HAL_GPIO_WritePin(MAIN_SRAD_GPIO_Port, MAIN_SRAD_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(IGNITE_GPIO_Port, IGNITE_Pin, GPIO_PIN_RESET);
+		}
+	}
+	if (!is_ignited) {
+		if (flight_state == FLIGHT_STATE_LAUNCH && ignition_count >= 5) {
+
+			HAL_GPIO_WritePin(IGNITE_GPIO_Port, IGNITE_Pin, GPIO_PIN_SET);
+			is_ignited = true;
+			set_buzzer_switching_intv(ignite_buzzer_intv_millis);
+		}
+	}
+
+	csv_format csv_data = {
+		.devicetime_ms = HAL_GetTick(),
+		.devicetime_us = micros(),
+		.flight_state = flight_state,
+		.acc_x = icm20948_last_agmt.acc_x,
+		.acc_y = icm20948_last_agmt.acc_y,
+		.acc_z = icm20948_last_agmt.acc_z,
+		.gyr_x = icm20948_last_agmt.gyr_x,
+		.gyr_y = icm20948_last_agmt.gyr_y,
+		.gyr_z = icm20948_last_agmt.gyr_z,
+		.mag_x = icm20948_last_agmt.mag_x,
+		.mag_y = icm20948_last_agmt.mag_y,
+		.mag_z = icm20948_last_agmt.mag_z,
+		.dmp_quat_w = (float)icm20948_last_dmp_quat.w,
+		.dmp_quat_x = (float)icm20948_last_dmp_quat.x,
+		.dmp_quat_y = (float)icm20948_last_dmp_quat.y,
+		.dmp_quat_z = (float)icm20948_last_dmp_quat.z,
+		.pressure = bmp581_last_data.pressure,
+		.temperature = bmp581_last_data.temperature,
+		.passive = passive
+	};
+
+	write_data_csv(csv_data);
+
+//	DEBUG_PRINT("flight state: %d\r\n", flight_state);
+
+#ifdef FC_DEBUG
+	if(millis() - hzprint_timer_millis > 3000){
+		int dt = millis() - hzprint_timer_millis;
+		hzprint_timer_millis = millis();
+		DEBUG_PRINT("IMU: %.1f(Hz)\r\n", imu_cnt/(float)dt*1000);
+		DEBUG_PRINT("BMP: %.1f(Hz)\r\n", bmp_cnt/(float)dt*1000);
+		DEBUG_PRINT("DMP: %.1f(Hz)\r\n", imu_dmp_cnt/(float)dt*1000);
+		imu_cnt = 0;
+		bmp_cnt = 0;
+		imu_dmp_cnt = 0;
+	}
+#endif
+
+}
